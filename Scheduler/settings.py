d7d13c4a6b1d2dc94123074325ef0aafb12106be
s-udaka <45505917+s-udaka@users.noreply.github.com>
2019-01-20 00:52:23 +0900

    jinja2によるテンプレート化（TemplateDoesNotExistエラーにより動作確認できず）

--- Scheduler/settings.py
+++ Scheduler/settings.py
@@ -53,10 +53,8 @@ ROOT_URLCONF = 'Scheduler.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates'
-        ,
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
