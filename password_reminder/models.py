e8f5c4f959ad2e7562bf10798064f894f9d34ff4
Nanawa12 <katsuya4713@outlook.jp>
2019-02-04 00:18:35 +0900

    DB登録部分を実装 テストは出来ていません

--- password_reminder/models.py
+++ password_reminder/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from datetime import datetime, timedelta
+import hashlib
 
 
 class PasswordReminder(models.Model):
-    id = models.IntegerField('パスワードリマインダーID', max_length=10, primary_key=True)
+    id = models.IntegerField('パスワードリマインダーID', primary_key=True)
     account_id = models.CharField('アカウントID', max_length=10)
     limit_date = models.DateTimeField('期限')
     onetime_pass_hash = models.CharField('ワンタイムハッシュ', max_length=128)
@@ -12,4 +14,17 @@ class PasswordReminder(models.Model):
 
 
 def create(self, account_id, onetime_password):
-    self.save()
+    # 期限, 登録日時, 更新日時に使用
+    now = datetime.now()
+
+    self.account_id = account_id
+
+    delta = timedelta(minutes=30)
+    self.limit_date = now + delta
+
+    self.onetime_pass_hash = hashlib.sha512(onetime_password)
+    self.delete_flg = "0"  # 未削除
+    self.register_date = now
+    self.update_date = now
+
+    self.create()
--- password_reminder/views.py
+++ password_reminder/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
 from django.http import HttpResponse
 from password_reminder.forms import PasswordReminderForm
+from password_reminder.models import PasswordReminder
 import secrets
 
 from django.template import RequestContext
-from password_reminder.models import PassWordReminder
 
 
 # Create your views here.
@@ -14,18 +13,27 @@ def reminder(request):
     if request.method == 'POST':
         req_email = request.POST['email']
         # TODO DB存在チェック
-        # result = search(req_email)
+        # result = filter(email_address="email_address")
         # if result is none:
         #     error_message = "メールアドレスが登録されていません。"
         #     return render(request, 'password_reminder/reminder.tpl',
         #                     {'form': form,
         #                      'error_message': error_message})
 
-        # ワンタイムパスワードの生成
-        onetime_password = secrets.token_hex()
+        # TODO アカウント情報の取得
+        # テストコード 本来はアカウントモデルから取得した値を使用
+        account_id = "99999"
+        # for account in result: account_id = account.id
 
-        # TODO DB登録
-        # create()
+
+        onetime_password = secrets.token_hex()
+        try:
+            PasswordReminder.create(account_id, onetime_password)
+        except:
+            error_message = "予期せぬエラーが発生しました。もう一度メールアドレスを送信してください。"
+            return render(request, 'password_reminder/reminder.tpl',
+                             {'form': form,
+                              'error_message': error_message})
 
         # TODO メールの送信
         # send_password_reminder_email(req_email)
