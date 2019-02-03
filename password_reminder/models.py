c32a1959531b2354dcb76532e181cbf79cdf01af
Nanawa12 <katsuya4713@outlook.jp>
2019-02-03 23:28:35 +0900

    登録テスト作成

--- password_reminder/models.py
+++ password_reminder/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 
-class PassWordReminder(models.Model):
-    id = models.CharField('パスワードリマインダーID', max_length=10, primary_key=True)
+class PasswordReminder(models.Model):
+    id = models.IntegerField('パスワードリマインダーID', max_length=10, primary_key=True)
     account_id = models.CharField('アカウントID', max_length=10)
     limit_date = models.DateTimeField('期限')
-    onetime_pass_hash = models.CharField('ワンタイムハッシュ', max_length=30)
-    delete_flg = models.CharField('削除フラグ', max_length=10)
+    onetime_pass_hash = models.CharField('ワンタイムハッシュ', max_length=128)
+    delete_flg = models.CharField('削除フラグ', max_length=1)
     register_date = models.DateTimeField('登録日時')
     update_date = models.DateTimeField('更新日時')
+
+
+def create(self, account_id, onetime_password):
+    self.save()
--- password_reminder/tests.py
+++ password_reminder/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from password_reminder.models import PasswordReminder
+import secrets
+import logging
 
-# Create your tests here.
+
+class PasswordReminderModelTests(TestCase):
+    def test_create_OK(self):
+        account_id = "test_ac_id"
+        onetime_password = secrets.token_hex()
+        try:
+            PasswordReminder.create(account_id, onetime_password)
+        except:
+            self.fail()
+
+    def test_create_param_null(self):
+        account_id = None
+        onetime_password = None
+        try:
+            PasswordReminder.create(account_id, onetime_password)
+            self.fail()
+        except Exception as e:
+            logger = logging.getLogger('development')
+            logger.info('登録失敗')
+            logger.error('=== エラー内容 ===')
+            logger.error('type:' + str(type(e)))
+            logger.error('args:' + str(e.args))
+            logger.error('message:' + e.message)
+
+    def test_create_account_id_maxlength_exceed(self):
+        account_id = "test_ac_id_"
+        onetime_password = secrets.token_hex()
+        try:
+            PasswordReminder.create(account_id, onetime_password)
+            self.fail()
+        except Exception as e:
+            logger = logging.getLogger('development')
+            logger.info('登録失敗')
+            logger.error('=== エラー内容 ===')
+            logger.error('type:' + str(type(e)))
+            logger.error('args:' + str(e.args))
+            logger.error('message:' + e.message)
+
+    def test_create_onetime_password_maxlength_exceed(self):
+        account_id = "test_ac_id_"
+        onetime_password = secrets.token_hex() + "1"
+        try:
+            PasswordReminder.create(account_id, onetime_password)
+        except Exception as e:
+            self.fail()
